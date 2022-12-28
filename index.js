27724192b3e7d0e1a0e7fa217d8872d238e8d109
arishorts <arieljschwartz@gmail.com>
2022-12-27 17:47:32 -0700

    Created HTML, CSS, and Javascript files

---
+++ index.js
@@ -0,0 +1,23 @@
+//WAS: const btnEL = document.getElementsByClassName("btn");
+//WAS: const containerEL = document.getElementsByTagName("a");
+//IS: queryselector is for getting CSS, getElements is for HTML
+const btnEl = document.querySelector(".btn");
+
+//WAS: object.addEventListener("mouseover", () => {
+//IS: forgot to change "Object" to btnEl
+btnEl.addEventListener("mouseover", (event) => {
+  const btnEl = document.querySelector(".btn");
+  //added 'event'
+  // useful to see what pageX does: console.log(event.pageX);
+  //const btnhoverEL = document.querySelector(".btn::before");
+  //btnhoverEL.style.backgroundColor = "blue";
+  const newposX = event.offsetX;
+  const newposY = event.offsetY;
+  //document.querySelector(".btn::before").style.top = posY;
+  //document.querySelector(".btn::before").style.left = posX;
+  document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
+  document.querySelector("html").style.setProperty("--topPos", newposY + "px");
+  //this also works because we can access variables, but not ::before
+  //btnEl.style.setProperty("--leftPos", newposX + "px");
+  //btnEl.style.setProperty("--topPos", newposY + "px");
+});
